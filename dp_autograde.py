55077e45cf4168ec3c914e1f64473ca27cd2cfa8
Maxxed <73586148+Maximeswd@users.noreply.github.com>
2023-09-11 16:59:44 +0200

    Add files via upload

---
+++ dp_autograde.py
@@ -0,0 +1,24 @@
+import numpy as np
+from collections import defaultdict
+
+def policy_eval_v(policy, env, discount_factor=1.0, theta=0.00001):
+    """
+    Evaluate a policy given an environment and a full description of the environment's dynamics.
+    
+    Args:
+        policy: [S, A] shaped matrix representing the policy.
+        env: OpenAI env. env.P represents the transition probabilities of the environment.
+            env.P[s][a] is a list of transition tuples (prob, next_state, reward, done).
+            env.nS is a number of states in the environment. 
+            env.nA is a number of actions in the environment.
+        theta: We stop evaluation once our value function change is less than theta for all states.
+        discount_factor: Gamma discount factor.
+    
+    Returns:
+        Vector of length env.nS representing the value function.
+    """
+    # Start with an all 0 value function
+    V = np.zeros(env.nS)
+    # YOUR CODE HERE
+    raise NotImplementedError
+    return np.array(V)
