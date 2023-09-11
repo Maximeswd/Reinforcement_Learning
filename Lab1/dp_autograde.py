c6f3b60c9800a98d708a33c1e5bc260eee8dd9d8
Maximeswd <maximedassen@yahoo.com>
2023-09-11 17:50:46 +0200

    Commit HW

--- Lab1/dp_autograde.py
+++ Lab1/dp_autograde.py
@@ -22,3 +22,45 @@ def policy_eval_v(policy, env, discount_factor=1.0, theta=0.00001):
     # YOUR CODE HERE
     raise NotImplementedError
     return np.array(V)
+
+def policy_eval_v(policy, env, discount_factor=1.0, theta=0.00001):
+    """
+    Evaluate a policy given an environment and a full description of the environment's dynamics.
+    
+    Args:
+        policy: [S, A] shaped matrix representing the policy.
+        env: OpenAI env. env.P represents the transition probabilities of the environment.
+            env.P[s][a] is a list of transition tuples (prob, next_state, reward, done).
+            env.nS is a number of states in the environment. 
+            env.nA is a number of actions in the environment.
+        theta: We stop evaluation once our value function change is less than theta for all states.
+        discount_factor: Gamma discount factor.
+    
+    Returns:
+        Vector of length env.nS representing the value function.
+    """
+    # Start with an all 0 value function
+    V = np.zeros(env.nS)
+    # YOUR CODE HERE
+    while True:
+        delta = 0  # Initialize the change in value function
+
+        for s in range(env.nS):
+            v_s = 0  # Initialize the new value of state s
+
+            # Calculate the expected value of state s under the current policy
+            for a, action_prob in enumerate(policy[s]):
+                for prob, next_state, reward, done in env.P[s][a]:
+                    v_s += action_prob * prob * (reward + discount_factor * V[next_state])
+
+            # Calculate the change in the value function for this state
+            delta = max(delta, abs(V[s] - v_s))
+
+            # Update the value function for state s
+            V[s] = v_s
+
+        # If the change in value function is smaller than theta, stop
+        if delta < theta:
+            break
+
+    return np.array(V)
